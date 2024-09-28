82e50e41cacb29c62e50ca1a49d513fa3a12d732
NS NooR <non377938@gmail.com>
2024-09-28 14:23:00 +0600

    modified

--- lib/view/screens/profile/profile.dart
+++ lib/view/screens/profile/profile.dart
@@ -1,5 +1,5 @@
+import 'package:ecommerceapp/constant/color.dart';
 import 'package:flutter/material.dart';
-import '../../../constant/color.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
